970a00467630c644f817eb5847115d7fa8b2939b
Akbar Hamaminatu <me@akbr.dev>
2021-05-30 10:36:29 +0700

    Fix header

--- resources/views/includes/home_navbar.blade.php
+++ resources/views/includes/home_navbar.blade.php
@@ -3,8 +3,6 @@
 
       <div class="logo me-auto">
         <h1><a href="">MAKIDAH</a></h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       </div>
 
       <nav id="navbar" class="navbar order-last order-lg-0">
@@ -14,7 +12,7 @@
           <li><a class="nav-link scrollto" href="#about">Tentang Kami</a></li>
           <li><a class="nav-link scrollto" href="#callUs">Hubungi Kami</a></li>
           @guest
-          <li><a class="nav-link scrollto" href="" onclick="event.preventDefault(); location.href='{{ url('login')}}';">Login</a></li>
+          <li><a class="nav-link scrollto" href="{{ url('login')}}">Login</a></li>
           @endguest
             @auth
                         <!-- Desktop Button -->
@@ -25,10 +23,9 @@
                                     Admin
                                 </a>
                             </form>
-                            <a class="nav-link scrollto" href="" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
+                            <a class="nav-link scrollto" href="{{ route('logout') }}">
                                 Log out
-                            </button>
+                            </a>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 @csrf
                             </form>
@@ -40,52 +37,3 @@
       </nav><!-- .navbar -->
     </div>
   </header><!-- End Header -->
-
-
-{{-- <header id="header" id="home" class="fixed-top d-flex align-items-center">
-    <div class="container d-flex align-items-center">
-
-      <div class="logo me-auto">
-        <h1><a href="">MAKIDAH</a></h1>
-      </div>
-            <nav id="nav-menu-container">
-                <ul class="nav-menu">
-                    <li class="menu-active"><a href="/">Beranda</a></li>
-                    <li><a href="#setorsampah">Setor Sampah </a></li>
-                    <li><a href="#contactUs">Hubungi Kami</a></li>
-                    <li><a href="#aboutUs">Tentang Kami</a></li>
-                    <li>
-                        @guest
-                        <!-- Desktop Button -->
-                        <form class="form-inline">
-                            <button style="margin-top: -7px; color: white" class="btn btn-login btn-navbar-right" type="button"
-                                onclick="event.preventDefault(); location.href='{{ url('login')}}';">
-                                Login
-                            </button>
-                        </form>
-                        @endguest
-
-                        @auth
-                        <!-- Desktop Button -->
-                        <div class="d-flex">
-                            <form class="form-inline" action="{{route('admin.index')}}" method="GET">
-                                @csrf
-                                <button style="margin-top: -7px; color: white" class="btn btn-login btn-navbar-right" type="submit">
-                                    Admin
-                                </button>
-                            </form>
-                            <button style="margin-top: -7px; color: white" class="btn" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
-                                Log out
-                            </button>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                        @endauth
-                    </li>
-                </ul>
-            </nav><!-- #nav-menu-container -->
-        </div>
-    </div>
-</header><!-- #header --> --}}
